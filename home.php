d1861bb380a21fb118934aaf412ce1045b3aa277
Milos Damjanovic <md20210126@student.fon.bg.ac.rs>
2024-10-13 04:40:41 +0200

    Izmenjen naziv faksa!

--- home.php
+++ home.php
@@ -19,7 +19,8 @@
         <!-- Header section -->
         <div class="jumbotron text-center">
             <h1>Prijava Kolokvijuma</h1>
-            <p>Fakultet organizacionih nauka</p>
+            <!-- Izmenjen naziv faksa -->
+            <p>Najbolji fakultet</p>
         </div>
 
 
