5709763eb4c4f35cea99af6971d4a8a31944600a
Milos Damjanovic <md20210126@student.fon.bg.ac.rs>
2024-10-13 04:36:47 +0200

    promenjeni title

--- home.php
+++ home.php
@@ -3,7 +3,8 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>FON: Prijava kolokvijuma</title>
+    <!--Promenjeni title  -->
+    <title>Prijava kolokvijuma</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="css/home.css">
 </head>
